b67c9761c973a8b5c74965034906b5c01e436923
ChosenPAPI <chosenpapi96@gmail.com>
2022-03-01 14:49:40 +0000

    update

---
+++ Functions.js
@@ -0,0 +1,17 @@
+// explanations
+
+
+var fullname = function (firstname, secondname){
+	return firstname + ' ' + secondname;
+}
+
+var area = function (width, height) {
+	return width * height;
+}
+var whatIsMyFavColor = function () {
+	return true;
+	return 'blue';
+}
+var doSomething = function() {
+	console.log("do Something baka!!!");
+}
---
+++ Variables.js
@@ -0,0 +1,13 @@
+// Explainations
+ var favColor = "Red";
+ var myArray = ['blue', "red", 'green'];
+ var nomOfFavColors = 3;
+ var hasGotFavColors = true;
+ var richObject = {
+firstName: "Mikasa",
+lastName: "Ackerman",
+favColors: ["blue", 'red', 'green'],
+yearsAlive: 102,
+isFemale: true,
+
+ };
