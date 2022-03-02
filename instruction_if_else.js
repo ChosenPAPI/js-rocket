efd8f298454d765658625d2bfb9a8404062ed4ec
ChosenPAPI <chosenpapi96@gmail.com>
2022-03-02 13:03:27 +0000

    update

---
+++ instruction_if_else.js
@@ -0,0 +1,9 @@
+// explanation huh, "||" == or , i think ...
+
+var number1 = 10;
+var number2 = 5;
+if (number1 > number2){
+	alert("Yes boi for sure!");}
+ else {
+ 	alert("Nah man you tripping lul !")
+ }	
